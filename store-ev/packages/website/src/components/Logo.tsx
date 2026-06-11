534740a2f4bd80a7c8ba821bb6a62436df561b6c
soychendo <chendoec@live.com>
2026-06-11 16:58:24 -0500

    Updated Features

--- store-ev/packages/website/src/components/Logo.tsx
+++ store-ev/packages/website/src/components/Logo.tsx
@@ -5,14 +5,14 @@ export const Logo: React.FC<React.JSX.IntrinsicElements['div']> = ({ className =
   const settings = useSettingsContext()
 
   return (
-    <div {...props} className={`text-black ${className}`}>
+    <div {...props} className={`text-black flex justify-center items-center ${className}`}>
       {
         settings.organization?.logo_url ? (
           <img alt={`${settings.organization?.name} logo`} src={settings.organization?.logo_url} className='h-10' />
         ) : (
           <>
             <EnchufeVirtualGlyph width={32} className='m-1 inline' aria-label='Enchufe Virtual logomark' />
-            <h1 className='text-lg font-bold inline'>Enchufe Virtual</h1>
+            <h1 className='text-lg font-bold inline'>Store</h1>
           </>
 
         )
